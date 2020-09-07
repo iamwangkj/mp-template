ccc6d386bb7d127403c1f6d0fa6ffa6ad5c85288
wangkj <ne.wkj@qq.com>
2020-09-08 01:08:28 +0800

    init

--- cloudFunctions/login/index.js
+++ cloudFunctions/login/index.js
@@ -11,11 +11,11 @@ cloud.init({
 
 /**
  * 这个示例将经自动鉴权过的小程序用户 openid 返回给小程序端
- *
+ * 
  * event 参数包含小程序端调用传入的 data
- *
+ * 
  */
-exports.main = (event, context) => {
+exports.main = async (event, context) => {
   console.log(event)
   console.log(context)
 
@@ -30,6 +30,7 @@ exports.main = (event, context) => {
     openid: wxContext.OPENID,
     appid: wxContext.APPID,
     unionid: wxContext.UNIONID,
-    env: wxContext.ENV
+    env: wxContext.ENV,
   }
 }
+
